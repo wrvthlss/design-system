7db9b1f4fa70749eb8354d4310ef1907aa6498a7
wrvthlss <wrgh.sushi333@passmail.net>
2024-04-29 15:44:12 -0400

    define mastheadcontainer

---
+++ src/components/MastheadContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MastheadLockup from './MastheadLockup';
+
+const MastheadContainer = () => (
+  <div className="relative w-[1280px] h-[832px] mx-auto bg-radial-gradient at-[50%_50%] from-masthead-start via-masthead-highlight to-masthead-end">
+    <MastheadLockup />
+  </div>
+);
+
+export default MastheadContainer;
