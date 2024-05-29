81c671c566de6d44e259c740f1400880c868851f
wrvthlss <wrgh.sushi333@passmail.net>
2024-05-29 07:48:31 -0400

    mobile nav

--- src/components/shared/GNav.js
+++ src/components/shared/GNav.js
@@ -172,21 +172,21 @@ export default function Example() {
                         <div className="fixed inset-0 bg-black bg-opacity-25" />
                     </Transition.Child>
 
-                    <div className="fixed inset-0 z-40 flex">
+                    <div className="fixed inset-0 z-40 flex justify-end">
                         <Transition.Child
                             as={Fragment}
                             enter="transition ease-in-out duration-300 transform"
-                            enterFrom="-translate-x-full"
+                            enterFrom="translate-x-full"
                             enterTo="translate-x-0"
                             leave="transition ease-in-out duration-300 transform"
                             leaveFrom="translate-x-0"
-                            leaveTo="-translate-x-full"
+                            leaveTo="translate-x-full"
                         >
                             <Dialog.Panel className="relative flex w-full max-w-xs flex-col overflow-y-auto bg-white pb-12 shadow-xl">
                                 <div className="flex px-4 pb-2 pt-5">
                                     <button
                                         type="button"
-                                        className="-m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400"
+                                        className="-m-2 pl-48 inline-flex items-center justify-center rounded-md p-2 text-gray-400"
                                         onClick={() => setMobileMenuOpen(false)}
                                     >
                                         <span className="sr-only">Close menu</span>
@@ -215,7 +215,6 @@ export default function Example() {
                                                         </div>
                                                     ))}
                                                 </div>
-
                                             </Tab.Panel>
                                         ))}
                                     </Tab.Panels>
@@ -253,6 +252,7 @@ export default function Example() {
                     </div>
                 </Dialog>
             </Transition.Root>
+
 
             {/* Hero section */}
             <div className="relative bg-gray-900">
